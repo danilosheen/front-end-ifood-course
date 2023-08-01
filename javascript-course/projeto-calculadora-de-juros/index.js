686d381ad1ea1d878f9dd5fea58f41fb8b71d744
danilosheen <c.danilo.f.silva@gmail.com>
2023-08-01 01:47:08 -0300

    refact: adição de funcionalidade atraso = 0

--- javascript-course/projeto-calculadora-de-juros/index.js
+++ javascript-course/projeto-calculadora-de-juros/index.js
@@ -1,21 +1,31 @@
-import rl from 'readline-sync'
+import rl from "readline-sync";
 
-console.log('---- Aplicação de Juros ----\n')
+console.log("---- Aplicação de Juros ----\n");
 
-let valorDevido = rl.question('Informe o valor devido: ')
-let diasDeAtraso = rl.question('Informe quantos dias se passaram desde o vencimento do boleto: ')
+let valorDevido = rl.question("Informe o valor devido: ");
+let diasDeAtraso = rl.question(
+  "Informe quantos dias se passaram desde o vencimento do boleto: "
+);
 
-console.log('\nValor original da dívida: R$', valorDevido)
-console.log('Dias atrasados: ', diasDeAtraso)
-
-if (diasDeAtraso > 15){
-    valorDevido = (valorDevido*1.10).toFixed(0)
-    console.log('Taxa de juros: 10%')
-
-}else{
-    valorDevido = (valorDevido*1.05).toFixed(0)
-    console.log('Taxa de juros: 5%')
+function mostraDiasAtrasados(valorDevido, diasDeAtraso) {
+  console.log("\nValor original da dívida: R$", valorDevido);
+  console.log("Dias atrasados: ", diasDeAtraso);
 }
-console.log('Valor total com juros: R$', valorDevido)
 
+function mostraValorDivida(valorDevido) {
+  console.log("Valor total com juros: R$", valorDevido);
+}
 
+if (diasDeAtraso == 0) {
+  console.log("Você está em dia!");
+} else if (diasDeAtraso > 15) {
+  mostraDiasAtrasados(valorDevido, diasDeAtraso);
+  valorDevido = (valorDevido * 1.1).toFixed(0);
+  console.log("Taxa de juros: 10%");
+  mostraValorDivida(valorDevido);
+} else {
+  mostraDiasAtrasados(valorDevido, diasDeAtraso);
+  valorDevido = (valorDevido * 1.05).toFixed(0);
+  console.log("Taxa de juros: 5%");
+  mostraValorDivida(valorDevido);
+}

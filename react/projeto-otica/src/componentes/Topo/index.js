a3fe91ed276f7d4ccb9d77230e8bfea0818f990b
danilosheen <c.danilo.f.silva@gmail.com>
2023-08-07 23:02:46 -0300

    feat: component topo

--- react/projeto-otica/src/componentes/Topo/index.js
+++ react/projeto-otica/src/componentes/Topo/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import Logo from '../../assets/logo.png'
+import React from "react";
+import Logo from "../../assets/logo.png";
+import "./topo.css";
 
-export default function Topo(){
-    return(
-        <header>
-            <div>
-                
-                <img src={Logo} alt="" />
-                <nav>
-                    <a href="../Conteudo/SecaoProdutos/">PRODUTOS</a>
-                    <a href="../Conteudo/SecaoSobre/">SOBRE</a>
-                    <a href="../Conteudo/SecaoContato/">CONTATO</a>
-                </nav>
-            </div>
-        </header>
-    )
+export default function Topo() {
+  return (
+    <header>
+      <div className="limitar-secao">
+        <img
+          src={Logo}
+          alt="imagem de um óculos à direita e o nome da ótica à esquerda"
+        />
+
+        <nav>
+          <a href="#produtos">PRODUTOS</a>
+          <a href="#sobre">SOBRE</a>
+          <a href="#contato">CONTATO</a>
+        </nav>
+      </div>
+    </header>
+  );
 }

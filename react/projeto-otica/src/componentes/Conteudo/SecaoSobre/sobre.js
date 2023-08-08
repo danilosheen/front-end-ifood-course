98d476ecf4ad0913542cde35a583f31c9b5a20e5
danilosheen <c.danilo.f.silva@gmail.com>
2023-08-07 23:03:20 -0300

    feat: component sobre

--- react/projeto-otica/src/componentes/Conteudo/SecaoSobre/sobre.js
+++ react/projeto-otica/src/componentes/Conteudo/SecaoSobre/sobre.js
@@ -1,9 +1,36 @@
 import React from "react";
+import ImgLoja from "../../../assets/loja.png";
+import ImgAtendimento from "../../../assets/atendimento.png";
+import "./sobre.css";
 
-export function SecaoSobre(){
-    return(
-        <div>
-            
+export default function SecaoSobre() {
+  return (
+    <section id="sobre" className="secao-sobre">
+      <div className="limitar-secao container-sobre">
+        <h3>QUEM SOMOS NÓS?</h3>
+        <p>
+          Fundada em 2001, em Nova Iguaçu - Rio de Janeiro, a Óticas vida
+          iniciou suas atividades focada no atendimento ao público de renda mais
+          baixa, sempre com o objetivo de proporcionar ao cliente bom
+          atendimento, qualidade e preço baixo.{" "}
+        </p>
+
+        <div className="box-cards">
+          <img src={ImgLoja} alt="img-loja" />
+
+          <div className="card">
+            <h4>Nossas Filiais</h4>
+            <p>Hoje temos mais de 20 filiais pelo Brasil e na América</p>
+          </div>
+
+          <div className="card">
+            <h4>Atendimento flexível</h4>
+            <p>Nossa equipe possui é treinada para te atender</p>
+          </div>
+
+          <img src={ImgAtendimento} alt="img-atend" />
         </div>
-    )
+      </div>
+    </section>
+  );
 }
